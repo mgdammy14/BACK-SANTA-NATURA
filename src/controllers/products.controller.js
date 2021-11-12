9cb7e1bcfa53e7c345dbfca39a8769a8a536a410
PROGRAMADOR <mgdammy14@gmail.com>
2021-11-12 12:22:07 -0500

    make it better

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,7 @@
 import { getConnection } from "../database/connection";
 
 export const getProducts = async (req, res) => {
+  
   const pool = await getConnection();
   
   const result = await pool.request().execute("USP_LISTADOPRODUCTOS");
